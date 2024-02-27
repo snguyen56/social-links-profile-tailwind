f9b67707e4e39911438ea38c2ff01631d06738fe
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2024-02-27 16:45:56 -0500

    finish mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,32 @@ import avatarImg from "./assets/images/avatar-jessica.jpeg";
 function App() {
   return (
     <main className="grid place-items-center h-screen w-screen bg-off-black text-white">
-      <div className="bg-dark-grey w-[327px] h-[578px] rounded-[13px] flex flex-col items-center">
+      <div className="bg-dark-grey w-[327px] h-[578px] rounded-[10px] flex flex-col items-center pt-[23px] px-[24px]">
         <img
-          className="rounded-full size-[90px]"
+          className="rounded-full size-[88px]"
           src={avatarImg}
           alt="avatar image"
         />
 
-        <h1 className="font-bold">Jessica Randall</h1>
-        <p className="text-green font-semibold">London, United Kingdom</p>
-        <p>"Front-end developer and avid reader."</p>
-        <div>
-          <button>GitHub</button>
-          <button>Frontend Mentor</button>
-          <button>LinkedIn</button>
-          <button>Twitter</button>
-          <button>Instagram</button>
+        <h1 className="font-semibold pt-7 text-[24px]">Jessica Randall</h1>
+        <p className="text-green font-semibold pt-1">London, United Kingdom</p>
+        <p className="pt-[24px]">"Front-end developer and avid reader."</p>
+        <div className="flex flex-col gap-[16px] w-full mt-[24px]">
+          <button className="bg-grey font-semibold rounded-lg py-[12px]">
+            GitHub
+          </button>
+          <button className="bg-grey font-semibold rounded-lg py-[12px]">
+            Frontend Mentor
+          </button>
+          <button className="bg-grey font-semibold rounded-lg py-[12px]">
+            LinkedIn
+          </button>
+          <button className="bg-grey font-semibold rounded-lg py-[12px]">
+            Twitter
+          </button>
+          <button className="bg-grey font-semibold rounded-lg py-[12px]">
+            Instagram
+          </button>
         </div>
       </div>
 
